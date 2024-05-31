96b48ae1bec5e4c700a202353818e25a0b548199
shanmugarajsekar <shanmugaraj2003@gmail.com>
2024-05-31 15:00:12 +0530

    .

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import header from "@/app/Images/main-pic.jpg"
 import { metadata } from './layout';
+import "@/app/globals.css";
+
 export default function Home() {
   return (
-   <div className=" bg-black header w-full h-[120vh]">
+    <div className="header-main">
+   <div className="  header w-full h-[120vh]">
 <p>Make Your Interior More  </p>
 <p>Minimalistic And Modern</p>
+   </div>
    </div>
   );
 }
